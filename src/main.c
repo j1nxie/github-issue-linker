9a8ac2714eb781303ad563ee869165d7bfa669d1
j1nxie <rylieeeeexd@gmail.com>
2023-07-20 23:25:45 +0700

    feat: anti-spam mechanism
    implements a basic anti-spam mechanism
    
    - user sends message with valid regex
    - user id + timestamp is pushed into a hashmap
    - if next valid message is sent, checks if timestamp delta is larger than 3 seconds
    - if yes, print links; if no, go next
    
    fixes #5.

--- src/main.c
+++ src/main.c
@@ -1,12 +1,31 @@
+#include "../dependencies/hashmap.c/hashmap.h"
 #include <concord/discord.h>
 #include <concord/log.h>
 #include <dotenv.h>
+#include <math.h>
 #include <pcre.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 pcre *REGEX;
+struct hashmap *MAP;
+
+struct user {
+    char *id;
+    u64unix_ms timestamp;
+} user;
+
+int user_compare(const void *a, const void *b, void *udata) {
+    const struct user *ua = a;
+    const struct user *ub = b;
+    return strcmp(ua->id, ub->id);
+}
+
+uint64_t user_hash(const void *item, uint64_t seed0, uint64_t seed1) {
+    const struct user *user = item;
+    return hashmap_sip(user->id, strlen(user->id), seed0, seed1);
+}
 
 void on_ready(struct discord *client, const struct discord_ready *event) {
     log_info("%s#%s connected!", event->user->username,
@@ -14,11 +33,32 @@ void on_ready(struct discord *client, const struct discord_ready *event) {
 }
 
 void on_message(struct discord *client, const struct discord_message *event) {
+    if (event->author->bot) {
+        return;
+    }
+
+    u64snowflake id = event->author->id;
+    int id_str_size = (ceil(log10(id)) + 1) * sizeof(char);
+    char id_str[id_str_size];
+    snprintf(id_str, id_str_size, "%zu", id);
+
     const char *msg_content = event->content;
     int ovector[30];
+    char msg[1024] = "";
 
     int offset = 0;
-    while (offset <= strlen(msg_content)) {
+    const struct user *user;
+    user = hashmap_get(MAP, &(struct user){.id = id_str});
+    if (user == NULL) {
+        hashmap_set(
+            MAP, &(struct user){.id = id_str, .timestamp = event->timestamp});
+    } else {
+        if (strcmp(user->id, id_str) == 0 &&
+            event->timestamp - user->timestamp <= 3000) {
+            return;
+        }
+    }
+    while (offset <= strlen(event->content)) {
         int rc = pcre_exec(REGEX, NULL, msg_content, strlen(msg_content),
                            offset, 0, ovector, 30);
         if ((rc > -1)) {
@@ -32,17 +72,20 @@ void on_message(struct discord *client, const struct discord_message *event) {
             offset = ovector[1];
 
             char buf[256];
-            snprintf(buf, sizeof(buf), "https://github.com/%s/%s/issues/%s",
+            snprintf(buf, sizeof(buf), "https://github.com/%s/%s/issues/%s\n",
                      username, repo, issue_num);
-
-            struct discord_create_message params = {.content = buf};
-            discord_create_message(client, event->channel_id, &params, NULL);
+            strcat(msg, buf);
         }
 
         if (rc == PCRE_ERROR_NOMATCH) {
             break;
         }
     }
+
+    hashmap_set(MAP,
+                &(struct user){.id = id_str, .timestamp = event->timestamp});
+    struct discord_create_message params = {.content = msg};
+    discord_create_message(client, event->channel_id, &params, NULL);
 }
 
 int main() {
@@ -51,6 +94,8 @@ int main() {
 
     const char *error;
     int erroffset;
+    MAP =
+        hashmap_new(sizeof(user), 0, 0, 0, user_hash, user_compare, NULL, NULL);
 
     REGEX = pcre_compile(
         "(?P<username>[a-z\\d](?:[a-z\\d]|-(?=[a-z\\d])){0,38})/(?P<"
@@ -68,5 +113,7 @@ int main() {
     discord_set_on_message_create(client, &on_message);
     discord_run(client);
 
+    pcre_free(REGEX);
+    hashmap_free(MAP);
     return 0;
 }
